33466767919b9b24708ddb29e97878691762328e
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2021-04-11 01:38:17 +0530

    Final Commit-1
    Still M.Tech, Ph.D,Gallery and staff remains.

---
+++ Cobweb/Ph. D. students.js
@@ -0,0 +1,8 @@
+function setURL(a){
+    if (a == 1) {
+        document.getElementById('iframe').src = "./Ongoing Ph.D Student's List.pdf";
+    }
+    else {
+        document.getElementById('iframe').src = "./Completed Ph.D.pdf";
+    }
+}
